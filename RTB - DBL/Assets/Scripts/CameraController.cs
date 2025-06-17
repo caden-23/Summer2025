7b81ac4a44f7f93adaaaa13bbaeec19a2ffc0c06
caden-23 <caden.lee@hgs.hiddengeniusproject.org>
2025-06-17 12:55:08 -0700

    4th Update
    This code helps me better understand how to make the camera follow the player.

---
+++ RTB - DBL/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+ // Calling the player's GameObject 
+ public GameObject player;
+
+ // The distance between the camera and the player.
+ private Vector3 offset;
+
+ // Start is called before the first frame update.
+ void Start()
+    {
+ // Calculate the initial position from the camera and the player 
+        offset = transform.position - player.transform.position; 
+    }
+
+ // This tells everything eles that its done 
+ void LateUpdate()
+    {
+ // Maintain the same offset between the camera and player throughout the game.
+        transform.position = player.transform.position + offset;  
+    }
+}
